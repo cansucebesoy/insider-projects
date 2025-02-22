0ae4b42dcddc9a887a936fe34e8f7d11d32d93ef
Cansu Cebesoy <cansucebesoy3@gmail.com>
2025-02-22 17:52:09 +0300

    add summary, characters and fav scene sections

---
+++ index.js
@@ -0,0 +1,23 @@
+const text =
+  "Imagine working at a small-town paper company where the boss thinks he's a comedic genius, the receptionist is stuck in a never-ending engagement, and the assistant regional manager (sorry, assistant to the regional manager) is obsessed with power, beets, and karate. Welcome to Dunder Mifflin Scranton, the weirdest, most chaotic, yet somehow lovable office you’ll ever see.";
+const speed = 25;
+let i = 0;
+
+function typeWriter() {
+  if (i < text.length) {
+    document.getElementById("animated-text").textContent += text.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    document.getElementById("animated-text").style.borderRight = "none";
+  }
+}
+
+window.onload = typeWriter;
+
+const image = document.querySelector(".main-photo img");
+
+window.addEventListener("load", () => {
+  image.classList.add("animate__animated", "animate__bounceIn");
+  image.style.opacity = 1;
+});
